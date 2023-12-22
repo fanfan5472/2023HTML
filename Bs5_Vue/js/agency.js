3b55cb2da258d3f1522e96d3b72f0063c66ad6ef
fanfan5472 <okok5472cc@yahoo.com.tw>
2023-12-22 23:59:08 +0800

    1222

--- Bs5_Vue/js/agency.js
+++ Bs5_Vue/js/agency.js
@@ -1,33 +1,42 @@
 // Agency Theme JavaScript
 
-(function($) {
-    "use strict"; // Start of use strict
+const stopBtn = getElementById("stop");
+const video = document.getElementById("YTvideo")
+stopBtn.addEventListener("click", function () {
+    video.contentWindow.postMessage('{"event":"command", "func":"stopVideo", "args":""}', '*');
+})
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
+    (function ($) {
+        "use strict"; // Start of use strict
 
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    });
+        // jQuery for page scrolling feature - requires jQuery Easing plugin
+        $('a.page-scroll').bind('click', function (event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: ($($anchor.attr('href')).offset().top - 50)
+            }, 1250, 'easeInOutExpo');
+            event.preventDefault();
+        });
 
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function(){ 
+        // Highlight the top nav as scrolling occurs
+        $('body').scrollspy({
+            target: '.navbar-fixed-top',
+            offset: 51
+        });
+
+        // Closes the Responsive Menu on Menu Item Click
+        $('.navbar-collapse ul li a').click(function () {
             $('.navbar-toggle:visible').click();
-    });
+        });
 
-    // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    })
+        // Offset for Main Navigation
+        $('#mainNav').affix({
+            offset: {
+                top: 100
+            }
+        })
 
-})(jQuery); // End of use strict
+
+
+    })(jQuery); // End of use strict
+
